0332a7660ded765f778e6c3821deb0de49df931c
sai kumar <saimandava1999@gmail.com>
2023-10-26 23:28:16 +0530

    third commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {
-      boxShadow: {
-        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
-      }
-    },
+    extend: {},
   },
-  variants: {},
   plugins: [],
 }
